a04456fc9ee2d24b85ffb8ab780b1a77061e2fd0
Rithwik <rithwikreddy.vancha@gmail.com>
2026-06-13 20:12:20 +0530

    Design Pattern

--- Module-1(Design patterns and SOLID principles)/Singleton pattern/SingletonPatternExample.java
+++ Module-1(Design patterns and SOLID principles)/Singleton pattern/SingletonPatternExample.java
@@ -1,9 +1,9 @@
 public class SingletonPatternExample{
     public static void main(String[] args){
         Logger logger1=Logger.getInstance();
-        logger1.log("First Log");
+        logger1.log("Log1:");
         Logger logger2=Logger.getInstance();
-        logger2.log("Second log");
+        logger2.log("log2:");
         System.out.println(logger1==logger2);
     }
 }
